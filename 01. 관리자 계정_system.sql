673f074dcfcba71465c4d34b2cd26d2e59a5436f
HStack <heosy8706@gmail.com>
2023-11-08 17:17:56 +0900

    20231108_Update1

---
+++ 01. 관리자 계정_system.sql
@@ -0,0 +1,51 @@
+-- 한줄 주석 : ctrl + /
+-- 한 줄(커서가 위치하는) 실행 : ctrl + enter
+
+/*
+    여러줄 주석 : alt + shift + c
+*/
+
+-- 나의 계정 보기
+SHOW user;
+
+
+-- 사용자 계정 조회
+SELECT * FROM DBA_USERS;
+
+
+-- 계정 만들기 : CREATE USER 사용자명 IDENTIFIED BY 비밀번호;
+-- 오라클 12버전부터 일반사용자는 c##을 붙여 이름 설정
+-- CREATE USER user1 IDENTIFIED BY 1234;
+CREATE USER user1 IDENTIFIED BY "1234";
+CREATE USER c##user2 IDENTIFIED BY user2;
+CREATE USER user3 IDENTIFIED BY user3;
+
+
+-- 사용자 이름에 c## 붙이는 것을 생략하는 방법
+ALTER SESSION set "_oracle_script" = true;
+
+
+-- 사용자 이름은 대소문자를 가리지 않음
+-- 사용자 생성 시 필수 : 사용자 생성, 권한 부여, 테이블 스페이스 부여
+CREATE USER aie IDENTIFIED BY aie;
+
+
+-- 사용자 권한 부여 : GRANT 권한1, 권한2 TO 사용자명;
+-- RESOURCE(데이터 가져오기 등), CONNECT(데이터베이스 연결)
+GRANT RESOURCE,CONNECT TO aie;
+
+
+-- 테이블 스페이스에 얼만큼의 영역을 할당할 것인지를 부여하는 방법
+-- QUOTA UNLIMITED : 제한 없이 할당
+ALTER USER aie DEFAULT TABLESPACE USERS QUOTA UNLIMITED ON USERS;
+
+
+-- 테이블 스페이스의 영역을 특정 용량만큼 할당하는 방법
+-- QUOTA 특정용량(단위) : 지정한 용량만큼만 할당
+ALTER USER user1 QUOTA 30M ON USERS;
+
+
+-- 사용자 삭제(테이블 없는 상태) : DROP USER 사용자명;
+-- 사용자 삭제(테이블 있는 상태) : DROP USER 사용자명 CASCADE;
+DROP USER c##user2;
+DROP USER user3;
---
+++ 02. aie 사용자 계정_select.sql
@@ -0,0 +1,103 @@
+-- 테이블 컬럼의 정보 조회 : SELECT
+/*
+    홑따옴표(') : 문자열일 때
+    쌍따옴표(")  : 컬럼명일 때
+    
+    <SELECT> : 데이터를 조회할 때 사용하는 구문
+        - RESULT SET : SELECT문을 통해 조회된 결과물(조회된 행의 집합)
+        - 표현 : SELECT 조회하려는_컬럼명, ... FROM 테이블명;
+            -> 전체 컬럼을 조회 : *
+*/
+
+-- 테이블 모든 컬럼 조회
+SELECT * FROM EMPLOYEE;
+
+SELECT * FROM DEPARTMENT;
+
+
+-- 테이블 특정 컬럼 조회
+-- EMPLOYEE 테이블에서 사번, 이름, 번호 조회
+SELECT EMP_ID, EMP_NAME, PHONE FROM EMPLOYEE;
+
+-- JOB 모든 컬럼 조회
+SELECT * FROM JOB;
+
+-- EMPLOYEE 테이블에서 사번, 이름, 급여, 입사일만 조회
+SELECT EMP_ID, EMP_NAME, SALARY, HIRE_DATE FROM EMPLOYEE;
+
+
+-- 문제 --
+-- JOB 테이블에서 직급명만 조회
+SELECT JOB_NAME FROM JOB;
+
+-- DEPARTMENT 테이블의 모든 컬럼 조회
+SELECT * FROM DEPARTMENT;
+
+-- DEPARTMENT 테이블의 부서코드, 부서명만 조회
+SELECT DEPT_ID, DEPT_TITLE FROM DEPARTMENT;
+
+-- EMPLOYEE 테이블에 사원명, 이메일, 전화번호, 입사일, 급여 조회
+SELECT EMP_NAME, EMAIL, PHONE, HIRE_DATE, SALARY FROM EMPLOYEE;
+
+
+/*
+    <컬럼값을 통한 산술연산>
+        - SELECT절 컬럼명 작성부분에 산술연산 가능(산술연산된 결과 조회)
+        - 산술연산 중 NULL이 존재하면 결과는 무조건 NULL, 별도 처리 가능
+*/
+
+-- EMPLOYEE에서 사원명, 사원의 연봉(급여 * 12) 조회
+SELECT EMP_NAME, SALARY * 12 FROM EMPLOYEE;
+
+-- EMPLOYEE에서 사원명, 급여, 보너스 조회
+SELECT EMP_NAME, SALARY, BONUS FROM EMPLOYEE;
+
+-- EMPLOYEE에서 사원명, 급여, 보너스, 연봉, 보너스를 포함한 연봉((급여 + (보너스 * 급여)) * 12) 조회
+SELECT EMP_NAME, SALARY, BONUS, SALARY * 12, (SALARY + (SALARY * BONUS)) * 12 FROM EMPLOYEE;
+
+-- EMPLOYEE에서 사원명, 입사일, 근무일수(오늘날짜 - 입사일) 조회
+-- DATE형 끼리도 연산 가능 : 결과값은 일 단위, 함수로 DATE 날짜처리하면 초단위로 관리할 수 있음
+-- 오늘 날짜 : SYSDATE
+SELECT EMP_NAME, HIRE_DATE, SYSDATE - HIRE_DATE FROM EMPLOYEE;
+
+
+/*
+    <컬럼명 별칭 지정>
+        - 산술연산시 산술에 들어간 수식 그대로 컴럼명이 됨, 이때 별칭을 부여하면 깔끔하게 처리
+        - 표현 : 컬럼명 별칭 /컬럼명 AS 별칭 / 컬럼명 "별칭" / 컬럼명 AS "별칭"
+        - 별칭에 띄어쓰기나 특수문자가 포함되면 반드시 쌍따옴표(")를 넣어줘야 함
+*/
+
+-- 예시 1
+SELECT EMP_NAME 사원명, SALARY AS 급여, BONUS "보너스", SALARY * 12 "연봉(원)", (SALARY + (SALARY * BONUS)) * 12 "총 소득" FROM EMPLOYEE;
+
+SELECT EMP_NAME AS 사원명, HIRE_DATE "입사일(일)", SYSDATE - HIRE_DATE AS "근무일수(일)" FROM EMPLOYEE;
+
+
+/*
+    <리터럴>
+        - 임의로 지정된 문자열(')
+        - SELECT절에 리터럴을 제시하면 마치 테이블 상에 존재하는 데이터처럼 조회 가능
+        - 조회된 RESULT SET의 모든 행에 반복적으로 출력
+*/
+
+-- EMPLOYEE에 사번, 사원명, 급여, 원 AS 단위조회
+-- 숫자 정렬 기본값(오른쪽), 문자열 정렬 기본값(왼쪽)
+SELECT EMP_ID, EMP_NAME, SALARY, '원' AS 단위 FROM EMPLOYEE;
+
+
+/*
+    <연결 연산자 : ||>
+        - 여러 컬럼값들을 마치 하나의 컬럼값인것처럼 연결하거나 컬럼값과 리터럴 연결
+*/
+
+-- EMPLOYEE에 사번, 사원명, 급여를 하나의 컬럼으로 조회
+SELECT EMP_ID || EMP_NAME || SALARY FROM EMPLOYEE;
+
+SELECT EMP_ID, EMP_NAME, SALARY, SALARY || '원' FROM EMPLOYEE;
+
+-- 홍길동의 월급은 900000원 입니다. 출력
+SELECT EMP_NAME || '의 월급은 ' || SALARY || '원 입니다.' FROM EMPLOYEE;
+
+-- 홍길동의 전화번호는 PHONE이고 이메일은 EMAIL 입니다.
+SELECT EMP_NAME || '의 전화번호는' || PHONE || '이고 이메일은 ' || EMAIL || '입니다.' FROM EMPLOYEE;
